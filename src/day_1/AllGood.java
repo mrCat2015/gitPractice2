e7a70191c65327a2e67be7949762fc2318559fb7
mrCat2015 <64213484+mrCat2015@users.noreply.github.com>
2020-04-28 19:20:04 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,6 +3,8 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("I hope everything is GOOD");
+        
+        // This line added in gitHub
     }
 
 }
