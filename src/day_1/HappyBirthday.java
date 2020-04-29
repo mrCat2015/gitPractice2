cc708973ed74dfc3cecee5f229c7df876e99f9c9
mrCat2015 <64213484+mrCat2015@users.noreply.github.com>
2020-04-29 12:42:25 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,5 +4,10 @@ public class HappyBirthday {
     public static void main(String[] args) {
         //Happy Life
         System.out.println("HappyBirthday B18");
+        
+        
+        System.out.println("HappyBirthday B18" again, this line is edit from GitHub);
+            
+            
     }
 }
